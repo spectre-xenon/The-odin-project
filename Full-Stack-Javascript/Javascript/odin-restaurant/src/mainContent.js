0ca0022040b4e5f14e15a32235c604d9115fd50d
Spectre-Xenon <62655598+spectre-xenon@users.noreply.github.com>
2023-09-18 16:56:06 +0300

    finished menu

--- Full-Stack-Javascript/Javascript/odin-restaurant/src/mainContent.js
+++ Full-Stack-Javascript/Javascript/odin-restaurant/src/mainContent.js
@@ -1,5 +1,6 @@
 import createHome from "./welcome/welcome";
 import createMenu from "./menu/menu";
+import createAbout from "./about/about";
 
 const contentContainer = document.createElement("div");
 contentContainer.id = "contentContainer";
@@ -8,7 +9,7 @@ contentContainer.appendChild(content);
 
 const create = () => {
   content.textContent = "";
-  content.appendChild(createMenu());
+  content.appendChild(createHome());
   return contentContainer;
 };
 
@@ -23,6 +24,8 @@ const add = (event) => {
     content.textContent = "";
     content.appendChild(createMenu());
   } else if (event.target.textContent === "About") {
+    content.textContent = "";
+    content.appendChild(createAbout());
   }
 };
 
